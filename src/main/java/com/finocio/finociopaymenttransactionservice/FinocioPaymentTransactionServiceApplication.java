f6307e5d1a5817093e7d43db595db6c07c4a8aa5
Adrian Herrera <64231248+Adrian-REH@users.noreply.github.com>
2023-04-14 19:04:16 +0200

    Add files via upload

---
+++ src/main/java/com/finocio/finociopaymenttransactionservice/FinocioPaymentTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.finocio.finociopaymenttransactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinocioPaymentTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinocioPaymentTransactionServiceApplication.class, args);
+	}
+
+}
